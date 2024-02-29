b61677306d5fc97a12f5a4eb74711095f7593d68
nankokit <nanko_k@mail.ru>
2024-03-01 00:50:14 +0300

    update return

--- src/main/java/com/nankokit/phonebook/controller/CountryController.java
+++ src/main/java/com/nankokit/phonebook/controller/CountryController.java
@@ -1,5 +1,6 @@
 package com.nankokit.phonebook.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +20,7 @@ public class CountryController {
     @GetMapping("/country-to-code")
     public ResponseEntity<Idd> getCode(@RequestParam("country") String country) {
         Idd idd = countryService.getCode(country);
-        return ResponseEntity.ok(idd);
+        return new ResponseEntity<>(idd, HttpStatus.OK);
     }
 
 }
